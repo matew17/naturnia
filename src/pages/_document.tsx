f559728082a78ba7de7d312dd6e1d15b2680bd04
Juan Diego Lopez <juandlopez@msn.com>
2021-09-30 21:22:41 -0500

    Add Livvic font

---
+++ src/pages/_document.tsx
@@ -0,0 +1,18 @@
+import Document, { Head, Html, Main, NextScript } from 'next/document';
+class MyDocument extends Document {
+  render() {
+    return (
+      <Html lang="es">
+        <Head>
+            <link href="https://fonts.googleapis.com/css2?family=Livvic&display=swap" rel="stylesheet"/>
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+
+export default MyDocument;
--- src/styles/globals.ts
+++ src/styles/globals.ts
@@ -25,7 +25,7 @@ export const GlobalStyle = createGlobalStyle`
         body {
             padding: 0;
             margin: 0;
-            font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Oxygen,
+            font-family: Livvic, -apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Oxygen,
                 Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue, sans-serif;
 
             h1 {
