3a2d45378403d6d5b9109570f65e59b28ba7b6a0
Mateo Castaño <matew-17@hotmail.com>
2021-10-06 16:57:46 -0500

    Removing empty line

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -7,7 +7,6 @@ export default function Layout({ children }: any) {
         <>
             <Navbar />
             <main>{children}</main>
-
             <WhatsApp width={60} height={60} />
             {/* <Footer /> */}
         </>
