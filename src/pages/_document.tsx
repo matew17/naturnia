65656e65955b18e735ef707e8071a7bd274b1cc2
Juan Diego Lopez <juandlopez@msn.com>
2021-11-03 22:48:46 -0500

    update conf to compile style components before serve pages

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,12 +1,25 @@
-import Document, { Head, Html, Main, NextScript } from "next/document";
+import Document, { DocumentContext, Head, Html, Main, NextScript } from 'next/document';
+import { ServerStyleSheet } from 'styled-components';
+
 class MyDocument extends Document {
+    static async getInitialProps({ renderPage }: DocumentContext) {
+        const sheet = new ServerStyleSheet();
+        const page = renderPage(
+            (App) => (props) => sheet.collectStyles(<App {...props} />)
+        );
+
+        //This code is necessary to avoid FOUC issues with styled-components
+        const styleTags = sheet.getStyleElement();
+        return { ...page, styleTags };
+    }
+
     render() {
         return (
-            <Html lang="es">
+            <Html lang='es'>
                 <Head>
                     <link
-                        href="https://fonts.googleapis.com/css2?family=Livvic&display=swap"
-                        rel="stylesheet"
+                        href='https://fonts.googleapis.com/css2?family=Livvic&display=swap'
+                        rel='stylesheet'
                     />
                 </Head>
                 <body>
